d81d735a33b1a3be1850fb0f0003b4bd2696a3f0
xmarre <54859656+xmarre@users.noreply.github.com>
2026-09-17 07:05:28 +0200

    Keep partitioned bridge opt-in

--- vdn_h3/__init__.py
+++ vdn_h3/__init__.py
@@ -1,13 +1,8 @@
 """VDN-H3 runtime package initialization."""
 
-from .partitioned_runtime import (
-    install_partitioned_external_sequence_bridge as _install_partitioned_external_sequence_bridge,
-)
 from .runtime_introspection import (
     install_runtime_introspection_bridge as _install_runtime_introspection_bridge,
 )
 
 _install_runtime_introspection_bridge()
-_install_partitioned_external_sequence_bridge()
 del _install_runtime_introspection_bridge
-del _install_partitioned_external_sequence_bridge
